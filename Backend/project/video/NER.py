5616d804d344e21ade19f840d12e31b2fe746c15
abdallah808 <76633971+AbdallahFawzyy@users.noreply.github.com>
2023-05-03 20:22:13 +0300

    integrated_front_back_trial

--- Backend/project/video/NER.py
+++ Backend/project/video/NER.py
@@ -2,7 +2,7 @@ import spacy
 
 
 def getParamsNER(text):
-    nlp = spacy.load('D:/Uni/Graduation-Project/Project/GP2023/Backend/project/video/NER/model')
+    nlp = spacy.load('E:/GP2023/Backend/project/video/NER/model')
 
     # text='Cut this video from 0 to 5 seconds'
     doc = nlp(text.lower())
