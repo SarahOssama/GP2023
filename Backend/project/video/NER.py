c133f0608aec9b07bf9de56975fd8a4251cb8a97
SarahOssama <SarahOssama>
2023-05-07 10:17:00 +0300

    🔧 Fixed BASE_DIR in NER.py

--- Backend/project/video/NER.py
+++ Backend/project/video/NER.py
@@ -1,10 +1,18 @@
 import spacy
 
+from django.conf import settings
+import os
+
+BASE_DIR = settings.BASE_DIR
+
+
 
 def getParamsNER(text):
-    nlp = spacy.load('E:/GP2023/Backend/project/video/NER/model')
+    nlp = spacy.load(os.path.join(BASE_DIR,'video/NER/model'))
+        #'D:/Uni/Graduation-Project/Project/GP2023/Backend/project/video/NER/model')
 
     # text='Cut this video from 0 to 5 seconds'
+    # print(BASE_DIR)
     doc = nlp(text.lower())
     # for entity in doc.ents:
     #     print(entity.text, entity.label_)
