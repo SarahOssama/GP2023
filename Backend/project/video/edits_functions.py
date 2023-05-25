16aaa2c4c9d918757f6c269e39d5a060082c9389
Sarah Osama <sarahosama28@gmail.com>
2023-05-25 17:08:33 +0300

    Fixed resizing

--- Backend/project/video/edits_functions.py
+++ Backend/project/video/edits_functions.py
@@ -17,28 +17,78 @@ def restoreClipSize(clip):
     # return cambiado
 
 
-def fitSizePadding(clip1, clip2):
-    # print(clip1.size,clip2.size)
-    # Compare the widths of the two clips
-    if clip1.w == clip2.w and clip1.h == clip2.h:
-        return clip1, clip2
-    if clip1.w > clip2.w:
-        # Resize clip2 to match the width of clip1 and add black padding
-        # clip2_resized = resize(clip2, width=clip1.w)
-        padding_size_W = (clip1.w - clip2.w) // 2
-        padding_size_H = (clip1.h - clip2.h) // 2
-        # print(padding_size_W,padding_size_H)
-        clip2 = clip2.fx(vfx.margin, left=padding_size_W, right=padding_size_W,
-                         top=padding_size_H, bottom=padding_size_H, color=(0, 0, 0))
-    else:
-        # Resize clip1 to match the width of clip2 and add black padding
-        # clip1_resized = resize(clip1, width=clip2.w)
-        padding_size_W = (clip2.w - clip1.w) // 2
-        padding_size_H = (clip2.h - clip1.h) // 2
-        # print(padding_size_W,padding_size_H)
-        clip1 = clip1.fx(vfx.margin, left=padding_size_W, right=padding_size_W,
-                         top=padding_size_H, bottom=padding_size_H, color=(0, 0, 0))
-    return clip1, clip2
+# def fitSizePadding(clip1, clip2):
+#     # print(clip1.size,clip2.size)
+#     # Compare the widths of the two clips
+#     if clip1.w == clip2.w and clip1.h == clip2.h:
+#         return clip1, clip2
+#     if clip1.w > clip2.w:
+#         # Resize clip2 to match the width of clip1 and add black padding
+#         # clip2_resized = resize(clip2, width=clip1.w)
+#         padding_size_W = (clip1.w - clip2.w) // 2
+#         padding_size_H = (clip1.h - clip2.h) // 2
+#         # print(padding_size_W,padding_size_H)
+#         clip2 = clip2.fx(vfx.margin, left=padding_size_W, right=padding_size_W,
+#                          top=padding_size_H, bottom=padding_size_H, color=(0, 0, 0))
+#     else:
+#         # Resize clip1 to match the width of clip2 and add black padding
+#         # clip1_resized = resize(clip1, width=clip2.w)
+#         padding_size_W = (clip2.w - clip1.w) // 2
+#         padding_size_H = (clip2.h - clip1.h) // 2
+#         # print(padding_size_W,padding_size_H)
+#         clip1 = clip1.fx(vfx.margin, left=padding_size_W, right=padding_size_W,
+#                          top=padding_size_H, bottom=padding_size_H, color=(0, 0, 0))
+#     return clip1, clip2
+
+def overlay_to_layout(clip, width, height, duration):
+    color_clip = ColorClip(size=(width, height),
+                           color=(0, 0, 0), duration=duration)
+
+    # Load the video or image clip to overlay
+    # VideoFileClip("Test.mp4")  # or ImageClip("path/to/image.jpg")
+    overlay_clip = clip
+
+    # Calculate the position to center the overlay clip
+    x_pos = (width - overlay_clip.size[0]) // 2
+    y_pos = (height - overlay_clip.size[1]) // 2
+
+    # Set the position of the overlay clip
+    overlay_clip = overlay_clip.with_position((x_pos, y_pos))
+
+    # Create the composite video clip by overlaying the color clip and the overlay clip
+    composite_clip = CompositeVideoClip([color_clip, overlay_clip])
+    return composite_clip
+
+
+def fitSizePadding(clip, height_default=1920, width_default=1080):
+    if clip.aspect_ratio > 1:
+        # clip is landscape
+        size_default = (width_default, height_default)
+        if clip.size == size_default:
+            return clip
+        if clip.size > size_default:
+            clip = clip.resize(
+                (width_default, width_default/clip.aspect_ratio))
+            clip = overlay_to_layout(
+                clip, width_default, height_default, clip.duration)
+        if clip.size < size_default:
+            clip = overlay_to_layout(
+                clip, width_default, height_default, clip.duration)
+
+    if clip.aspect_ratio < 1:
+        # clip is portrait
+
+        size_default = (width_default, height_default)
+        # both are portrait Aspect ration< 1 -> Large - Resize Small add to colour clip
+        if clip.size == size_default:
+            return clip
+        if clip.size > size_default:
+            clip = clip.resize(size_default)
+        if clip.size < size_default:
+            clip = overlay_to_layout(
+                clip, width_default, height_default, clip.duration)
+
+    return clip
 
 
 def edit_video_duration(clip, start_time, end_time, effect_type):
