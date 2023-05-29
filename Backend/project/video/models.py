8557a38b68f74b3b772a8422a39ad3b1218c4c6b
Sarah Osama <sarahosama28@gmail.com>
2023-05-29 13:57:24 +0300

    ➕Added Voice Over , 🔧 Fix requirements.txt , 🔧fixed directories

--- Backend/project/video/models.py
+++ Backend/project/video/models.py
@@ -2,31 +2,28 @@ import os
 from django.db import models
 from django.utils import timezone
 from .validators import file_size
-import json 
+import json
 
 # Create your models here.
 
-def get_file_path(instance, filename):
-    ext = filename.split('.')[-1]
-    filename = f'{instance.id}.{ext}'
-    return f'videos/{instance.id}'
 
 def upload_to(instance, filename):
     now = timezone.now()
     base, extension = os.path.splitext(filename.lower())
     milliseconds = now.microsecond // 1000
-    return f"video/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
+    return f"videoIn/{now:%Y%m%d%H%M%S}{milliseconds}{extension}"
+
 
 class Video(models.Model):
-    title = models.CharField(max_length=200,null=True,blank=True)
-    media_file=models.FileField(upload_to=upload_to,validators=[file_size])
+    title = models.CharField(max_length=200, null=True, blank=True)
+    media_file = models.FileField(upload_to=upload_to, validators=[file_size])
     height = models.IntegerField(null=True, blank=True)
     width = models.IntegerField(null=True, blank=True)
     new_insert = models.FileField(upload_to='inserts/', null=True, blank=True)
 
     def __str__(self):
         return self.media_file.name
-    versions = models.CharField(max_length=200,null=True, blank=True)
+    versions = models.CharField(max_length=200, null=True, blank=True)
 
     def push_to_versions_stack(self, value):
         if self.versions:
@@ -58,6 +55,7 @@ class Video(models.Model):
         self.save()
 
         return value
+
     def get_versions_stack_count(self):
         if self.versions:
             # Deserialize the stack from JSON to a list
